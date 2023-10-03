2f553fab41470d04d76437fd0f2326c9042c3d02
reynandaptr <reynandapp1997@gmail.com>
2023-10-03 16:57:39 +0700

    feat: UserAnalyticsResponse

---
+++ src/types/analytics.ts
@@ -0,0 +1,12 @@
+import {LoginProvider} from '@prisma/client';
+
+export type UserAnalyticsResponse = {
+  id: number
+  name: string | null
+  email: string
+  is_verified: boolean
+  provider: LoginProvider
+  login_count: number
+  signup_timestamp: Date
+  last_session_timestamp: Date
+}
